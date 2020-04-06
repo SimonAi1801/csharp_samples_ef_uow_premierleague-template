0ee4960ef3797f6c0d2908150b4d797a393afeaa
Simon Aichmayr <56634821+SimonAi1801@users.noreply.github.com>
2020-04-06 20:13:48 +0200

    update initalMigration.cs

---
+++ source/PremierLeague.Persistence/Migrations/20200404140356_Initialise.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PremierLeague.Persistence.Migrations
+{
+    public partial class Initialise : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
